caacb3c8515709f8d42d97734d24fcd604bba392
BenDotWillcox <benjamin.willcox2@gmail.com>
2024-05-22 13:56:43 -0700

    Update database connection to Heroku

--- db_config.py
+++ db_config.py
@@ -3,12 +3,7 @@ import os
 from sqlalchemy import create_engine
 
 def get_db_connection():
-    user = os.getenv('DB_USER')  # Database username
-    password = os.getenv('DB_PASSWORD')  # Database password
-    host = os.getenv('DB_HOST')  # Database host
-    port = os.getenv('DB_PORT')  # Database port
-    database = os.getenv('DB_NAME')  # Database name
-
-    connection_string = f'postgresql://{user}:{password}@{host}:{port}/{database}'
-    engine = create_engine(connection_string)
+    database_url = os.getenv("DATABASE_URL")
+    engine = create_engine(database_url)
     return engine.connect()
+
