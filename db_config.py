a93eac71f24d756a1735af6164bb1093920297c4
BenDotWillcox <benjamin.willcox2@gmail.com>
2024-05-23 14:51:30 -0700

    commit secret update

--- db_config.py
+++ db_config.py
@@ -1,15 +1,15 @@
-# db_config.py
-import os
+import streamlit as st
 from sqlalchemy import create_engine
 
 def get_db_connection():
-    user = 'DB_USER'
-    password = 'DB_PASSWORD'
-    host = 'DB_HOST'
-    port = 'DB_PORT'
-    database = 'DB_NAME'
+    user = st.secrets["DB"]["DB_USER"]
+    password = st.secrets["DB"]["DB_PASSWORD"]
+    host = st.secrets["DB"]["DB_HOST"]
+    port = st.secrets["DB"]["DB_PORT"]
+    database = st.secrets["DB"]["DB_NAME"]
 
     connection_string = f'postgresql://{user}:{password}@{host}:{port}/{database}'
     engine = create_engine(connection_string)
     return engine.connect()
+
 
